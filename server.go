36b909e722cdd8d7ed61d7015e052202acaa9427
Noe Garcia <garcia.noav@gmail.com>
2024-04-08 11:06:23 -1000

    feat: added more content for contact and visuals

--- server.go
+++ server.go
@@ -6,12 +6,6 @@ import (
 	"net/http"
 )
 
-/*
-func handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hi there, I love %s!", request.URL.Path[1:])
-}
-*/
-
 func main() {
 
 	// define a new file server rooted in the static directory
